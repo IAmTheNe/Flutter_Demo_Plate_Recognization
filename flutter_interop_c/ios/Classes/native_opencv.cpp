1169c5946c86232a332044a09ffaa6fc3bec37da
Trần Nguyên Thế <89185876+IAmTheNe@users.noreply.github.com>
2023-06-04 14:34:45 +0700

    Tối ưu nhận diện biển số xe

--- flutter_interop_c/ios/Classes/native_opencv.cpp
+++ flutter_interop_c/ios/Classes/native_opencv.cpp
@@ -9,84 +9,64 @@ using namespace std;
 
 // static LicenseDetector* detector = nullptr;
 
-extern "C" {
+extern "C"
+{
     // Attributes to prevent 'unused' function from being removed and to make it visible
     __attribute__((visibility("default"))) __attribute__((used))
-    const char* version() {
+    const char *
+    version()
+    {
         return CV_VERSION;
     }
 
-    // __attribute__((visibility("default"))) __attribute__((used))
-    // void initDetector(uint8_t* markerPngBytes) {
-    //     if (detector != null) {
-    //         delete detector;
-    //         detector = nullptr;
-    //     }
+    __attribute__((visibility("default"))) __attribute__((used)) void process_image(char *inputImagePath, char *outputImagePath)
+    {
+        // Đọc ảnh biển số xe
+        Mat image = imread(inputImagePath);
 
-    //     vector<uint8_t> buffer(markerPngBytes, markerPngBytes);
-    //     Mat marker = imdecode(buffer, IMREAD_COLOR);
+        // Chuyển đổi sang ảnh grayscale
+        Mat gray;
+        cvtColor(image, gray, COLOR_BGR2GRAY);
 
-    //     detector = new LicenseDetector(marker);
-    // }
+        // Tiền xử lý ảnh
+        Mat processedImage;
+        bilateralFilter(gray, processedImage, 11, 17, 17);
 
-    // __attribute__((visibility("default"))) __attribute__((used))
-    // void destroyDetector() {
-    //     if (detector != null) {
-    //         delete detector;
-	// 		detector = nullptr;
-    //     }
-    // }
+        // Phát hiện cạnh
+        Mat edged;
+        Canny(processedImage, edged, 30, 200);
 
-     __attribute__((visibility("default"))) __attribute__((used))
-     void process_image(char* inputImagePath, char* outputImagePath) {
-        Mat input = imread(inputImagePath, IMREAD_GRAYSCALE);
-        Mat filter, grey_image, edged;
-
+        // Tìm contours
         vector<vector<Point>> contours;
-        
-        cvtColor(input, grey_image, COLOR_GRAY2BGR);
-        bilateralFilter(grey_image, filter ,11, 17, 17);
-
-        Canny(filter, edged, 30, 200);
-
         findContours(edged.clone(), contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
 
-        Mat input1 = input.clone();
-        drawContours(input1, contours, -1, Scalar(0, 255, 0), 3);
+        // Sắp xếp contours theo diện tích giảm dần
+        sort(contours.begin(), contours.end(), [](const vector<Point> &c1, const vector<Point> &c2)
+             { return contourArea(c1) > contourArea(c2); });
 
-        sort(contours.begin(), contours.end(), [](const vector<Point>& c1, const vector<Point>& c2) {
-            return cv::contourArea(c1) > cv::contourArea(c2);
-        });
-        vector<vector<Point>> cnts(contours.begin(), contours.begin() + 30);
+        // Chọn 30 contours có diện tích lớn nhất
+        vector<vector<Point>> cnts(contours.begin(), contours.begin() + min(30, static_cast<int>(contours.size())));
 
-        Mat input2 = input.clone();
-        drawContours(input2, cnts, -1, Scalar(0, 255, 0), 3);
-        
-       
-        int i = 7;
-        Point2f screenCnt[4];
-        for (size_t idx = 0; idx < cnts.size(); idx++) {
-            double perimeter = arcLength(cnts[idx], true);
+        // Tìm contour hình vuông
+        Rect boundingRect;
+        for (const auto &contour : cnts)
+        {
+            double perimeter = arcLength(contour, true);
             double epsilon = 0.018 * perimeter;
             vector<Point> approx;
-            approxPolyDP(cnts[idx], approx, epsilon, true);
+            approxPolyDP(contour, approx, epsilon, true);
 
-            if (approx.size() == 4) {
-                for (int j = 0; j < 4; j++) {
-                    screenCnt[j] = approx[j];
-                }
-
-                Rect b = boundingRect(cnts[idx]);
-                Mat new_img = input(b);
-                imwrite(outputImagePath, new_img);
-
-                i++;
+            if (approx.size() == 4)
+            {
+                boundingRect = cv::boundingRect(contour);
                 break;
             }
         }
 
-       drawContours(input, vector<vector<Point>>{vector<Point>(screenCnt, screenCnt + 4)}, -1, Scalar(0, 255, 0), 3);
+        // Cắt ảnh theo contour hình vuông
+        Mat new_image = image(boundingRect);
 
-     }
+        // Lưu ảnh đầu ra
+        imwrite(outputImagePath, new_image);
+    }
 }
-
